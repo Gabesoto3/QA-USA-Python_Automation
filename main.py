9d99a5ea43cef25c91894454159a871cc1bb78b9
Gabe <gabriel.soto3@hotmail.com>
2024-11-16 18:45:56 -0500

    Commit Project 7

--- main.py
+++ main.py
@@ -1,2 +1,48 @@
+import data
+import helpers
 
-class TestUrbanRoutes:
+# Check if the Urban Routes server is working
+if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+    print("Connected to the Urban Routes server")
+else:
+    print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+# Define functions for the Urban Routes application
+def test_set_route(self):
+    # Add in S8
+    pass
+
+def test_select_plan(self):
+    # Add in S8
+    pass
+
+def test_fill_phone_number(self):
+    # Add in S8
+    pass
+
+def test_fill_card(self):
+    # Add in S8
+    pass
+
+def test_comment_for_driver(self):
+    # Add in S8
+    pass
+
+def test_order_blanket_and_handkerchiefs(self):
+    # Add in S8
+    pass
+
+def test_order_2_ice_creams(self):
+    # Add in S8
+    for _ in range(2):  # Loop to iterate twice
+        # Todo in S8
+        pass
+
+
+def test_car_search_model_appears(self):
+    # Add in S8
+    pass
+
+def test_driver_info_appears(self):
+    # Add in S8
+    pass
